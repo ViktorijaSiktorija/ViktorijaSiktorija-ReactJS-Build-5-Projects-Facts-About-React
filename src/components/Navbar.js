9888192e672287c0c52451f2d63b3fb55896381a
Vika <siktorija.viktorija@gmail.com>
2025-02-16 23:04:52 +0100

    Facts about React-2

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <>
+      <header className="header-bar">
+        <nav className="nav-bar">
+          <img src="logo192.png" className="nav-logo" />
+          <span className="nav-text">ReactFacts</span>
+        </nav>
+      </header>
+    </>
+  );
+}
